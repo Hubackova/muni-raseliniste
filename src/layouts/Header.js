776a61c9d1117f4eab49c449177df50668c1b40c
Hubackova <lenka.h@atlas.cz>
2018-12-17 14:40:31 +0100

    images update

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,76 +1,148 @@
 import React, {Fragment} from 'react'
 import styled from 'styled-components'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
+import {StaticQuery, graphql} from 'gatsby'
+
 import ImgSlider from './ImgSlider'
 import HeaderTop from './HeaderTop'
 
 import logo from '../images/logo2.png'
 import mainLogo from '../images/mainLogo.png'
 
-import hp01 from '../images/homepage/hp01.jpg'
-import hp02 from '../images/homepage/hp02.jpg'
-import hp03 from '../images/homepage/hp03.jpg'
-import hp04 from '../images/homepage/hp04.jpg'
-import hp05 from '../images/homepage/hp05.jpg'
-import hp06 from '../images/homepage/hp06.jpg'
-import hp07 from '../images/homepage/hp07.jpg'
-import hp08 from '../images/homepage/hp08.jpg'
-import hp09 from '../images/homepage/hp09.jpg'
-import hp10 from '../images/homepage/hp10.jpg'
-import shp01 from '../images/smallhomepage/shp01small.jpg'
-import shp02 from '../images/smallhomepage/shp02small.jpg'
-import shp03 from '../images/smallhomepage/shp03small.jpg'
-import shp04 from '../images/smallhomepage/shp04small.jpg'
-import shp05 from '../images/smallhomepage/shp05small.jpg'
-import shp06 from '../images/smallhomepage/shp06small.jpg'
-import shp07 from '../images/smallhomepage/shp07small.jpg'
-
-const imgs = [hp01, hp02, hp03, hp04, hp05, hp06, hp07, hp08, hp09, hp10] //do props
-const sImgs = [shp01, shp02, shp03, shp04, shp05, shp06, shp07] //do props
-
-const Header = ({isIndex, generalData}) => {
-  return (
-    <Fragment>
-      <HeaderTop generalData={generalData} isIndex={isIndex}/>
-      <SubContainer>
-        <LeftSide>
-          <LeftSideWrapper>
-            <MainLogo src={mainLogo} alt="mainLogo" />
-          </LeftSideWrapper>
-          <LeftSideWrapper>
-            <LogoText>
-              {generalData.mainHeader}
-              <br />
-            </LogoText>
-            <LogoTextSmall>{generalData.muni}</LogoTextSmall>
-          </LeftSideWrapper>
-          <LeftSideWrapper>
-            <Logo src={logo} alt="logo" />
-          </LeftSideWrapper>
-        </LeftSide>
-        {isIndex && (
-          <RightSide>
-            <ImgSlider imgs={imgs}/>
-          </RightSide>
-        )}
-        {!isIndex && (
-          <RightSide>
-            <ImgSlider imgs={sImgs}/>
-          </RightSide>
-        )}
-      </SubContainer>
-    </Fragment>
-  )
-}
+const Header = ({isIndex, generalData}) => (
+  <StaticQuery
+    query={query}
+    render={data => {
+      const imgs = [
+        data.image1.childImageSharp.sizes,
+        data.image2.childImageSharp.sizes,
+        data.image3.childImageSharp.sizes,
+        data.image4.childImageSharp.sizes,
+        data.image5.childImageSharp.sizes,
+        data.image6.childImageSharp.sizes,
+        data.image7.childImageSharp.sizes,
+        data.image8.childImageSharp.sizes,
+        data.image9.childImageSharp.sizes,
+        data.image10.childImageSharp.sizes
+      ]
+      const sImgs = [
+        data.simage1.childImageSharp.sizes,
+        data.simage2.childImageSharp.sizes,
+        data.simage3.childImageSharp.sizes,
+        data.simage4.childImageSharp.sizes,
+        data.simage5.childImageSharp.sizes,
+        data.simage6.childImageSharp.sizes,
+        data.simage7.childImageSharp.sizes,
+      ]
+      return (
+        <Fragment>
+          <HeaderTop generalData={generalData} isIndex={isIndex} />
+          <SubContainer>
+            <LeftSide>
+              <LeftSideWrapper>
+                <MainLogo src={mainLogo} alt="mainLogo" />
+              </LeftSideWrapper>
+              <LeftSideWrapper>
+                <LogoText>
+                  {generalData.mainHeader}
+                  <br />
+                </LogoText>
+                <LogoTextSmall>{generalData.muni}</LogoTextSmall>
+              </LeftSideWrapper>
+              <LeftSideWrapper>
+                <Logo src={logo} alt="logo" />
+              </LeftSideWrapper>
+            </LeftSide>
+            {isIndex && (
+              <RightSide>
+                <ImgSlider imgs={imgs} />
+              </RightSide>
+            )}
+            {!isIndex && (
+              <RightSide>
+                <ImgSlider imgs={sImgs} />
+              </RightSide>
+            )}
+          </SubContainer>
+        </Fragment>
+      )
+    }}
+  />
+)
 
 export default Header
 
 Header.propTypes = {
   language: PropTypes.object,
   isIndex: PropTypes.bool,
-  generalData: PropTypes.object,
+  generalData: PropTypes.object
 }
 
+export const fluidImage = graphql`
+  fragment fluidImage on File {
+    childImageSharp {
+      sizes(maxWidth: 1240 ) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+  }
+`
+
+export const query = graphql`
+  query {
+    image1: file(relativePath: {eq: "homepage/hp01.jpg"}) {
+      ...fluidImage
+    }
+    image2: file(relativePath: {eq: "homepage/hp02.jpg"}) {
+      ...fluidImage
+    }
+    image3: file(relativePath: {eq: "homepage/hp03.jpg"}) {
+      ...fluidImage
+    }
+    image4: file(relativePath: {eq: "homepage/hp04.jpg"}) {
+      ...fluidImage
+    }
+    image5: file(relativePath: {eq: "homepage/hp05.jpg"}) {
+      ...fluidImage
+    }
+    image6: file(relativePath: {eq: "homepage/hp06.jpg"}) {
+      ...fluidImage
+    }
+    image7: file(relativePath: {eq: "homepage/hp07.jpg"}) {
+      ...fluidImage
+    }
+    image8: file(relativePath: {eq: "homepage/hp08.jpg"}) {
+      ...fluidImage
+    }
+    image9: file(relativePath: {eq: "homepage/hp09.jpg"}) {
+      ...fluidImage
+    }
+    image10: file(relativePath: {eq: "homepage/hp10.jpg"}) {
+      ...fluidImage
+    }
+    simage1: file(relativePath: {eq: "smallhomepage/shp01small.jpg"}) {
+      ...fluidImage
+    }
+    simage2: file(relativePath: {eq: "smallhomepage/shp02small.jpg"}) {
+      ...fluidImage
+    }
+    simage3: file(relativePath: {eq: "smallhomepage/shp03small.jpg"}) {
+      ...fluidImage
+    }
+    simage4: file(relativePath: {eq: "smallhomepage/shp04small.jpg"}) {
+      ...fluidImage
+    }
+    simage5: file(relativePath: {eq: "smallhomepage/shp05small.jpg"}) {
+      ...fluidImage
+    }
+    simage6: file(relativePath: {eq: "smallhomepage/shp06small.jpg"}) {
+      ...fluidImage
+    }
+    simage7: file(relativePath: {eq: "smallhomepage/shp07small.jpg"}) {
+      ...fluidImage
+    }
+  }
+`
 
 const SubContainer = styled.div`
   display: flex;
@@ -86,11 +158,11 @@ const LeftSide = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-image: linear-gradient(110deg, #578724 33.33%, #51811D 33.33%, #51811D 66.66%, #3E7404 66.66%);
+  background-image: linear-gradient(110deg, #578724 33.33%, #51811d 33.33%, #51811d 66.66%, #3e7404 66.66%);
   @media (max-width: 600px) {
     flex-wrap: wrap;
   }
-  `
+`
 
 const LeftSideWrapper = styled.div`
   justify-content: center;
--- src/layouts/ImgSlider.js
+++ src/layouts/ImgSlider.js
@@ -1,13 +1,11 @@
 import React from 'react'
-
 import PropTypes from 'prop-types';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import {Carousel} from 'react-responsive-carousel'
+import Img from "gatsby-image"
 
-// const imgs = [hp01, hp02, hp03, hp04, hp05, hp06, hp07, hp08, hp09, hp10] //do props
-// todo find another component for img slideshow
 const Images = ({imgs}) => {
-  const images = imgs.map((i, index) => <img key={index} src={i} />)
+  const images = imgs.map((i, index) => <Img key={index} sizes={i} />)
   return (
     <Carousel showThumbs={false} autoPlay={true} infiniteLoop={true} showStatus={false} interval={7000}>
       {images}
